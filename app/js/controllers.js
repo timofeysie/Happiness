30cb9035e7072ea9a3a803a83dd934eaf6edb9de
Timothy Curchod <timofeyc@hotmail.com>
2015-02-20 16:08:48 +1100

    Added home screen and end alert.

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -9,9 +9,11 @@ phonecatControllers.controller('MainController', ['$scope', 'Gallery',
     $scope.galleries = Gallery.query();
   }]);
 
-phonecatControllers.controller('DefaultController', ['$scope', '$routeParams', 'HappyOrSadService',
-  'PositiveService', 'NegativeService',
-  function($scope, $routeParams, HappyOrSadService, PositiveService, NegativeService) {
+phonecatControllers.controller('DefaultController', ['$scope', '$routeParams', 
+  '$location', 'HappyOrSadService',
+  'PositiveService', 'NegativeService', '$timeout',
+  function($scope, $routeParams, $location, HappyOrSadService, 
+    PositiveService, NegativeService, $timeout) {
     // default settings for button one positive, button two negative
     $scope.imageOneUrl = '';
     $scope.imageTwoUrl = '';
@@ -35,25 +37,14 @@ phonecatControllers.controller('DefaultController', ['$scope', '$routeParams', '
       getPicture('positive_two');
       $scope.answer = 'two';
     }
+    $scope.showImageOne = true;
+    $scope.showImageTwo = true;
 
+    // Called when the user chooses an image
     $scope.increment = function(type) {
       var next_randy = Math.random();
       if ((type == 'one' && $scope.answer == 'one') || (type == 'two' && $scope.answer == 'two')) {
-        $scope.button_one = 'OK';
-        $scope.button_two = 'OK';
-        $scope.count = $scope.count +1;
-        console.log("Correct: count "+$scope.count);
-        // correct response: next round button one is happy, two is sad
-        if ((next_randy*100) > 50) {
-          $scope.answer = 'one';
-          console.log('button_one is happy'); 
-          getPicture('positive_one');
-        } else {   
-          // Button one is sad, button two is happy
-          $scope.answer = 'two';
-          console.log('button_two is happy'); 
-          getPicture('positive_two');
-        } 
+        hideImage(type);
       } else
       { // response for trying to choose the negative, change the negative button   
         if (($scope.answer == 'one')) { 
@@ -63,9 +54,31 @@ phonecatControllers.controller('DefaultController', ['$scope', '$routeParams', '
           }
           console.log("not OK");
       }
-      if ($scope.count > 18) {
+    }
+
+    // Set up the images for the next round
+    function nextRound(picture_type)
+    {
+      var next_randy = Math.random();
+      $scope.button_one = 'OK';
+      $scope.button_two = 'OK';
+      $scope.count = $scope.count +1;
+      console.log("Correct: count "+$scope.count);
+      // correct response: next round button one is happy, two is sad
+      if ((next_randy*100) > 50) {
+        $scope.answer = 'one';
+        console.log('button_one is happy'); 
+        getPicture('positive_one');
+      } else {   
+        // Button one is sad, button two is happy
+        $scope.answer = 'two';
+        console.log('button_two is happy'); 
+        getPicture('positive_two');
+      }
+      if ($scope.count > 17) {
         console.log("The End");
         alert("The End");
+        $location.path( "/main" );
       }
     }
 
@@ -78,6 +91,7 @@ phonecatControllers.controller('DefaultController', ['$scope', '$routeParams', '
           angular.forEach(response, function (item) {
             if (item.order == $scope.count) {
               $scope.imageOneUrl = item.imageUrl;
+              $scope.showImageOne = true;
               console.log('A. +1 '+picture_type);
             }
           });
@@ -86,6 +100,7 @@ phonecatControllers.controller('DefaultController', ['$scope', '$routeParams', '
           angular.forEach(response, function (item) {
             if (item.order == $scope.count) {
               $scope.imageTwoUrl = item.imageUrl;
+              $scope.showImageTwo = true;
               console.log('A. -2 '+picture_type);
             }
           });
@@ -95,6 +110,7 @@ phonecatControllers.controller('DefaultController', ['$scope', '$routeParams', '
           angular.forEach(response, function (item) {
             if (item.order == $scope.count) {
               $scope.imageTwoUrl = item.imageUrl;
+              $scope.showImageTwo = true;
               console.log('B. -2 '+picture_type);
             }
           });
@@ -103,11 +119,31 @@ phonecatControllers.controller('DefaultController', ['$scope', '$routeParams', '
           angular.forEach(response, function (item) {
             if (item.order == $scope.count) {
               $scope.imageOneUrl = item.imageUrl;
+              $scope.showImageOne = true;
               console.log('B. -1 '+picture_type);
             }
           });
         });
       } 
+    }
+    /* Since the type is the answer, we must hide the opposite image. */
+    function hideImage(type)
+    {
+      if (type == 'two')
+      {
+        $scope.showImageOne = false;
+      } else
+      {
+        $scope.showImageTwo = false;
+      }
+      $timeout(callTimeout, 2000);
+    }
+
+    function callTimeout(type) {
+      console.log("Timeout1 occurred");
+      $scope.showImageOne = false;
+      $scope.showImageTwo = false;
+      nextRound(type);
     }
   }
 ]);
