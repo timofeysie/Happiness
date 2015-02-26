21d5a33c7ccef840189b79c2c2f9fe92e6824948
Timothy Curchod <timofeyc@hotmail.com>
2015-02-26 11:03:11 +1100

    Added an about the developer page with responsive views.

--- app/js/app.js
+++ app/js/app.js
@@ -1,13 +1,14 @@
 'use strict';
-
 /* App Module */
-
 var phonecatApp = angular.module('phonecatApp', [
   'ngRoute',
+  'ngResource',
   'phonecatAnimations',
   'phonecatControllers',
   'phonecatFilters',
-  'phonecatServices'
+  'phonecatServices',
+  'phonecatApp.profileController',
+  'phonecatApp.profileServices'
 ]);
 
 phonecatApp.config(['$routeProvider',
@@ -25,7 +26,154 @@ phonecatApp.config(['$routeProvider',
         templateUrl: 'partials/about.html',
         controller: 'AboutController'
       }).
+      when('/developer', {
+        templateUrl: 'partials/developer.html',
+        controller: 'ProfileController'
+      }).
       otherwise({
         redirectTo: '/main'
       });
   }]);
+
+phonecatApp.directive("responsiveImage",
+    ["$log","$window",
+        function ($log, $window) {
+            return {
+                restrict:'E',
+                replace:true,
+                scope: {
+                    'respalt': '@imagealt',
+                    'respsrc': '@imagesrc'
+                },
+                template: '<img class="profile-image"' +
+                    'ng-src="{{modifiedsrc}}" alt="{{respalt}}"/>',
+                link : function(scope, element, attribute){
+                    scope.width = $window.outerWidth;
+                    scope.$watch("width",function(newWidth,oldWidth){
+                        $log.log("New width of window : ",newWidth);
+                        if(newWidth <= 400){
+                            scope.modifiedsrc = scope.respsrc +"?s=80";
+                        }else if(newWidth >400 && newWidth <=767){
+                            scope.modifiedsrc = scope.respsrc +"?s=150";
+                        }else{
+                            scope.modifiedsrc = scope.respsrc +"?s=250";
+                        }
+                    });
+                    angular.element($window).bind('resize',function(){
+                        //Asking AngularJS to run digest cycle
+                        scope.$apply(function(){
+                            scope.width = $window.outerWidth;
+                        })
+                    });
+                }};
+        }])
+
+.directive("responsiveHeader",
+    ["$log","$window",
+        function ($log, $window) {
+            return {
+                restrict:'E',
+                replace:true,
+                scope:{
+                    'respText':'@targettext'
+                },
+                template: "<p class='{{deviceSize}}'> {{respText}} </p>",
+                link : function(scope, element, attribute){
+                    scope.deviceSize = "largeDevice";
+                    scope.width = $window.outerWidth;
+                    scope.$watch("width",function(newWidth,oldWidth){
+                        if(newWidth <= 400){
+                            scope.deviceSize = "smallDevice"
+                        }else if(newWidth >400 && newWidth <=767){
+                            scope.deviceSize = "mediumDevice";
+                        }else{
+                            scope.deviceSize = "largeDevice";
+                        }
+                    });
+                    angular.element($window).bind('resize',function(){
+                        scope.$apply(function(){
+                            scope.width = $window.outerWidth;
+                        });
+                    });
+                }};
+        }])
+
+.directive("responsiveParagraph",
+    ["$log","$window",
+        function ($log, $window) {
+            return {
+                restrict:'E',
+                replace:true,
+                scope:{
+                    'respPara':'@targetpara'
+                },
+                template: "<p class='aboutme {{paragraphSize}}'> {{respPara}} </p>",
+                link : function(scope, element, attribute){
+                    scope.paragraphSize = "largePara";
+                    scope.width = $window.outerWidth;
+                    scope.$watch("width",function(newWidth,oldWidth){
+                        if(newWidth <= 400){
+                            scope.paragraphSize = "smallPara"
+                        }else if(newWidth >400 && newWidth <=767){
+                            scope.paragraphSize = "mediumPara";
+                        }else{
+                            scope.paragraphSize = "largePara";
+                        }
+                    });
+                    angular.element($window).bind('resize',function(){
+                        scope.$apply(function(){
+                            scope.width = $window.outerWidth;
+                        });
+                    });
+               }};
+        }])
+
+.directive("responsiveList",
+    ["$log","$window",
+        function ($log, $window) {
+            return {
+                restrict:'E',
+                replace:true,
+                scope:{
+                    'itemList':'=targetlist'
+                },
+                template: '<div class="item-list-container">'+
+                          '<ol>'+
+                          '<li ng-class="{smallText:isMorePresent}" ng-repeat="item in itemDisplayList">{{item}}</li>'+
+                          '</ol>'+
+                          '<button class="show-more" ng-show="isMorePresent" ' +
+                           'ng-click="showMore(itemDisplayList)"><span>More...</span></button>'+
+                          '</div>',
+                link : function(scope, element, attribute){
+                        scope.isMorePresent = false;
+
+                        scope.$watch("itemList",function(newItemList,oldItemList){
+                            scope.itemList = newItemList;
+                            scope.height = $window.outerHeight;
+                            scope.itemDisplayList = scope.itemList;
+                        },true);
+                        scope.$watch("height",function(newHeight,oldHeight){
+                            var listLength = angular.isDefined(scope.itemList)?
+                                             scope.itemList.length : 0 ;
+                            if(newHeight <400 && listLength >2){
+                                scope.isMorePresent = true;
+                                scope.itemDisplayList = scope.itemList.slice(0,2);
+                            }else if(newHeight >= 400 && newHeight <700 && listLength >3){
+                                scope.isMorePresent = true;
+                                scope.itemDisplayList = scope.itemList.slice(0,3);
+                            }else{
+                                scope.isMorePresent = false;
+                                scope.itemDisplayList = scope.itemList;
+                            }
+                        });
+                        angular.element($window).bind('resize',function(){
+                            scope.$apply(function(){
+                                scope.height = $window.outerHeight;
+                            });
+                        });
+                        scope.showMore = function(initalList){
+                           scope.itemDisplayList = scope.itemList;
+                           scope.isMorePresent = false;
+                        }
+                }};
+        }]);
---
+++ app/js/profileController.js
@@ -0,0 +1,25 @@
+'use strict';
+angular.module("phonecatApp.profileController", [])
+.controller('ProfileController',
+    function ($scope, $rootScope, $route, ProfileServices, $log) {
+    var professionalDetail = ProfileServices.getProfessionalDetail(),
+        personalDetail = ProfileServices.getPersonalDetail();
+    $scope.professional = {};
+    $scope.personal = {};
+    //Default menu button selected to true
+    $scope.selected = true;
+    /*Calls the Angular service to load  professional JSON data*/
+    professionalDetail.get(function(jsonData){
+       $scope.professional = jsonData;
+       console.log("pro data "+$scope.professional.length);
+    });
+    /*Calls the Angular service to load  personal JSON data*/
+    personalDetail.get(function(jsonData){
+       $scope.personal = jsonData;
+    });
+    /*Method to change the user selection*/
+    $scope.getDetail=function(event){
+      $scope.selected = !$scope.selected;
+    };
+})
+
